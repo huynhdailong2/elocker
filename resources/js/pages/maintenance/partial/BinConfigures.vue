035e140521f2d81a422d713636d7037863dbf9c3
Đỗ Cao Long <long@myspa.vn>
2023-10-11 16:38:26 +0700

    fix add

--- resources/js/pages/maintenance/partial/BinConfigures.vue
+++ resources/js/pages/maintenance/partial/BinConfigures.vue
@@ -8,7 +8,9 @@
           <th v-if="spare.has_serial_no">Serial Number</th>
           <th v-if="spare.has_charge_time">Charge Time</th>
           <th v-if="spare.has_calibration_due">Calibration Due/Inspection</th>
-          <th v-if="spare.has_load_hydrostatic_test_due">Load/Hydrostatic Test Due</th>
+          <th v-if="spare.has_load_hydrostatic_test_due">
+            Load/Hydrostatic Test Due
+          </th>
           <th v-if="spare.has_expiry_date">Expiry Date</th>
         </thead>
         <tbody>
@@ -18,36 +20,44 @@
               <input
                 type="text"
                 class="input"
-                :class="{'error': errors.has(`${item.scope}.batch_no`)}"
+                :class="{ error: errors.has(`${item.scope}.batch_no`) }"
                 name="batch_no"
                 data-vv-as="batch no"
                 placeholder="Batch Number"
                 :data-vv-scope="`${item.scope}`"
                 v-model.trim="item.batch_no"
-                v-validate="`${batchNoRule}`" >
-              <span class="invalid-feedback" v-if="errors.has(`${item.scope}.batch_no`)">
-                {{ errors.first(`${item.scope}.batch_no`)}}
+                v-validate="`${batchNoRule}`"
+              />
+              <span
+                class="invalid-feedback"
+                v-if="errors.has(`${item.scope}.batch_no`)"
+              >
+                {{ errors.first(`${item.scope}.batch_no`) }}
               </span>
             </td>
             <td v-if="spare.has_serial_no">
               <input
                 type="text"
                 class="input"
-                :class="{'error': errors.has(`${item.scope}.serial_no`)}"
+                :class="{ error: errors.has(`${item.scope}.serial_no`) }"
                 name="serial_no"
                 data-vv-as="serial no"
                 placeholder="Serial Number"
                 :data-vv-scope="`${item.scope}`"
                 v-model.trim="item.serial_no"
-                v-validate="`${serialNoRule}`" >
-              <span class="invalid-feedback" v-if="errors.has(`${item.scope}.serial_no`)">
-                {{ errors.first(`${item.scope}.serial_no`)}}
+                v-validate="`${serialNoRule}`"
+              />
+              <span
+                class="invalid-feedback"
+                v-if="errors.has(`${item.scope}.serial_no`)"
+              >
+                {{ errors.first(`${item.scope}.serial_no`) }}
               </span>
             </td>
             <td v-if="spare.has_charge_time">
               <div class="choose-time">
                 <label class="checkbox-container">
-                  <input type="checkbox" v-model="item.has_charge_time">
+                  <input type="checkbox" v-model="item.has_charge_time" />
                   <span class="checkmark"></span>
                 </label>
 
@@ -56,9 +66,10 @@
                   name="charge_time"
                   v-validate="'required'"
                   :data-vv-scope="`${item.scope}`"
-                  :class="{'error': errors.has(`${item.scope}.charge_time`)}"
+                  :class="{ error: errors.has(`${item.scope}.charge_time`) }"
                   data-vv-as="charge time"
-                  v-if="item.has_charge_time" />
+                  v-if="item.has_charge_time"
+                />
 
                 <!-- <datepicker
                   format="dd/MM/yyyy"
@@ -72,73 +83,97 @@
                   data-vv-as="charge time"
                   v-if="item.has_charge_time" /> -->
               </div>
-              <span class="invalid-feedback" v-if="errors.has(`${item.scope}.charge_time`)">
+              <span
+                class="invalid-feedback"
+                v-if="errors.has(`${item.scope}.charge_time`)"
+              >
                 {{ errors.first(`${item.scope}.charge_time`) }}
               </span>
             </td>
             <td v-if="spare.has_calibration_due">
               <div class="choose-date">
                 <label class="checkbox-container">
-                  <input type="checkbox" v-model="item.has_calibration_due">
+                  <input type="checkbox" v-model="item.has_calibration_due" />
                   <span class="checkmark"></span>
                 </label>
                 <datepicker
                   format="dd/MM/yyyy"
                   input-class="form-control date-selector"
                   v-model="item.calibration_due"
-                  :disabled-dates="{to: yesterday}"
+                  :disabled-dates="{ to: yesterday }"
                   name="calibration_due"
                   v-validate="'required'"
                   :data-vv-scope="`${item.scope}`"
-                  :class="{'error': errors.has(`${item.scope}.calibration_due`)}"
+                  :class="{
+                    error: errors.has(`${item.scope}.calibration_due`),
+                  }"
                   data-vv-as="calibration due"
-                  v-if="item.has_calibration_due" />
+                  v-if="item.has_calibration_due"
+                />
               </div>
-              <span class="invalid-feedback" v-if="errors.has(`${item.scope}.calibration_due`)">
+              <span
+                class="invalid-feedback"
+                v-if="errors.has(`${item.scope}.calibration_due`)"
+              >
                 {{ errors.first(`${item.scope}.calibration_due`) }}
               </span>
             </td>
             <td v-if="spare.has_load_hydrostatic_test_due">
               <div class="choose-date">
                 <label class="checkbox-container">
-                  <input type="checkbox" v-model="item.has_load_hydrostatic_test_due">
+                  <input
+                    type="checkbox"
+                    v-model="item.has_load_hydrostatic_test_due"
+                  />
                   <span class="checkmark"></span>
                 </label>
                 <datepicker
                   format="dd/MM/yyyy"
                   input-class="form-control date-selector"
                   v-model="item.load_hydrostatic_test_due"
-                  :disabled-dates="{to: yesterday}"
+                  :disabled-dates="{ to: yesterday }"
                   name="load_hydrostatic_test_due"
                   v-validate="'required'"
                   :data-vv-scope="`${item.scope}`"
-                  :class="{'error': errors.has(`${item.scope}.load_hydrostatic_test_due`)}"
+                  :class="{
+                    error: errors.has(
+                      `${item.scope}.load_hydrostatic_test_due`
+                    ),
+                  }"
                   data-vv-as="calibration due"
-                  v-if="item.has_load_hydrostatic_test_due" />
+                  v-if="item.has_load_hydrostatic_test_due"
+                />
               </div>
-              <span class="invalid-feedback" v-if="errors.has(`${item.scope}.load_hydrostatic_test_due`)">
+              <span
+                class="invalid-feedback"
+                v-if="errors.has(`${item.scope}.load_hydrostatic_test_due`)"
+              >
                 {{ errors.first(`${item.scope}.load_hydrostatic_test_due`) }}
               </span>
             </td>
             <td v-if="spare.has_expiry_date">
               <div class="choose-date expiry">
                 <label class="checkbox-container">
-                  <input type="checkbox" v-model="item.has_expiry_date">
+                  <input type="checkbox" v-model="item.has_expiry_date" />
                   <span class="checkmark"></span>
                 </label>
                 <datepicker
                   format="dd/MM/yyyy"
                   input-class="form-control date-selector"
                   v-model="item.expiry_date"
-                  :disabled-dates="{to: yesterday}"
+                  :disabled-dates="{ to: yesterday }"
                   name="expiry_date"
                   v-validate="'required'"
                   :data-vv-scope="`${item.scope}`"
-                  :class="{'error': errors.has(`${item.scope}.expiry_date`)}"
+                  :class="{ error: errors.has(`${item.scope}.expiry_date`) }"
                   data-vv-as="expiry date"
-                  v-if="item.has_expiry_date" />
+                  v-if="item.has_expiry_date"
+                />
               </div>
-              <span class="invalid-feedback" v-if="errors.has(`${item.scope}.expiry_date`)">
+              <span
+                class="invalid-feedback"
+                v-if="errors.has(`${item.scope}.expiry_date`)"
+              >
                 {{ errors.first(`${item.scope}.expiry_date`) }}
               </span>
             </td>
@@ -151,7 +186,7 @@
 <style lang="scss" scoped>
 .bin-configures {
   .table-scroller {
-    min-height: 250px;
+    // min-height: 250px;
     table {
       // margin-bottom: 80px;
       tr {
@@ -191,7 +226,7 @@
           // }
           ::v-deep .time-picker {
             .dropdown {
-                top: -145px;
+              top: -145px;
             }
           }
         }
@@ -203,7 +238,7 @@
           // }
           ::v-deep .time-picker {
             .dropdown {
-                top: calc(2.2em + 2px);
+              top: calc(2.2em + 2px);
             }
           }
         }
@@ -213,69 +248,71 @@
 }
 </style>
 <script>
-import moment from 'moment'
-import { time } from 'lodash'
-import Datepicker from 'vuejs-datepicker'
-import Utils from 'common/Utils'
-import Const from 'common/Const'
-import VueTimepicker from 'vue2-timepicker';
-import 'vue2-timepicker/dist/VueTimepicker.css';
+import moment from "moment";
+import { time } from "lodash";
+import Datepicker from "vuejs-datepicker";
+import Utils from "common/Utils";
+import Const from "common/Const";
+import VueTimepicker from "vue2-timepicker";
+import "vue2-timepicker/dist/VueTimepicker.css";
 
 export default {
   components: {
     Datepicker,
-    VueTimepicker
+    VueTimepicker,
   },
 
   props: {
     spare: {
       type: Object,
-      default: () => {}
+      default: () => {},
     },
 
     data: {
       type: Array,
-      required: true
-    }
+      required: true,
+    },
   },
 
   computed: {
-    yesterday () {
-      return moment().subtract(1, 'days').toDate()
+    yesterday() {
+      return moment()
+        .subtract(1, "days")
+        .toDate();
     },
 
-    batchNoRule () {
+    batchNoRule() {
       switch (this.spare?.type) {
         case Const.ITEM_TYPE.CONSUMABLE.value:
-          return ''
+          return "";
       }
 
-      return 'required'
+      return "required";
     },
 
-    serialNoRule () {
+    serialNoRule() {
       switch (this.spare?.type) {
         case Const.ITEM_TYPE.CONSUMABLE.value:
-          return ''
+          return "";
       }
 
-      return 'required'
-    }
+      return "required";
+    },
   },
 
   methods: {
-    async validateData () {
+    async validateData() {
       await Utils.asyncForEach(this.data, async (item, index) => {
-        this.errors.clear(item.scope)
-        await this.$validator.validate(`${item.scope}.*`)
-      })
+        this.errors.clear(item.scope);
+        await this.$validator.validate(`${item.scope}.*`);
+      });
 
       if (this.errors.any()) {
-        return false
+        return false;
       }
 
-      return true
-    }
-  }
-}
+      return true;
+    },
+  },
+};
 </script>
--- resources/js/pages/maintenance/partial/EditBinModal.vue
+++ resources/js/pages/maintenance/partial/EditBinModal.vue
@@ -5,11 +5,15 @@
     height="auto"
     :clickToClose="false"
     @before-open="beforeOpen"
-    class="custom-modal">
+    class="custom-modal"
+  >
     <div class="header">
-      <div class="title" v-if="bin">Edit Bin: {{ bin.cluster_name }} - {{ bin.shelf_name }} - {{ bin.row }} - {{ bin.bin }}</div>
+      <div class="title" v-if="bin">
+        Edit Bin: {{ bin.cluster_name }} - {{ bin.shelf_name }} -
+        {{ bin.row }} - {{ bin.bin }}
+      </div>
       <div class="close" @click.stop="close">
-        <img src="/images/icons/icon-cancel.svg" width="22">
+        <img src="/images/icons/icon-cancel.svg" width="22" />
       </div>
     </div>
     <div class="content">
@@ -20,47 +24,49 @@
 <style lang="scss" scoped>
 .content {
   width: 100%;
+  max-height: 80vh;
+  overflow-y: scroll;
 }
 </style>
 <script>
-import BinForm from './BinForm'
+import BinForm from "./BinForm";
 
 export default {
   components: {
-    BinForm
+    BinForm,
   },
 
   props: {
     name: {
       type: String,
-      default: 'edit-bin-modal'
-    }
+      default: "edit-bin-modal",
+    },
   },
 
-  data () {
+  data() {
     return {
       bin: null,
-      callback: null
-    }
+      callback: null,
+    };
   },
 
   methods: {
-    beforeOpen (event) {
-      const { params } = event
-      this.bin = params?.bin
-      this.callback = params?.callback
+    beforeOpen(event) {
+      const { params } = event;
+      this.bin = params?.bin;
+      this.callback = params?.callback;
     },
 
-    close () {
+    close() {
       if (this.callback) {
-        this.callback()
+        this.callback();
       }
-      this.$modal.hide(this.name)
+      this.$modal.hide(this.name);
     },
 
-    onItemSaved () {
-      this.close()
-    }
-  }
-}
+    onItemSaved() {
+      this.close();
+    },
+  },
+};
 </script>
