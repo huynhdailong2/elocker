048b5945e5751493e4a47287a1d2852a349e54c9
Absolute Tech <tom.hua@absolutech.com.sg>
2023-11-18 00:06:21 +0700

    updated! add config for node server for pm2

--- nodejs/client.js
+++ nodejs/client.js
@@ -4,13 +4,15 @@ const cluster_id = 1;
 const jwt = require('jsonwebtoken');
 var token = jwt.sign({'cluster_id': cluster_id}, process.env.SECRET_KEY);
 
-var io = require('socket.io-client')('http://localhost:8889', {
+var io = require('socket.io-client')('http://18.139.169.50:8889', {
     query: {token}
 });
 io.on('connect',()=>{
     console.log('Server connected')
     io.emit('message', {'cluster_id': cluster_id});
 });
+io.on('connect_error', err => console.log(err))
+io.on('connect_failed', err => console.log(err))
 io.on('disconnect', () => {
     console.log('Server disconnected!')
 })
